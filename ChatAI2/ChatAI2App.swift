52c8211207811b27230c6350ec9410a63266fcc1
Herman <hermantrym@gmail.com>
2022-12-29 14:43:58 +0700

    Initial Commit

---
+++ ChatAI2/ChatAI2App.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAI2App.swift
+//  ChatAI2
+//
+//  Created by admin on 29/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatAI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
