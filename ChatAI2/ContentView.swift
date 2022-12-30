314ff47949fb59e34acc579149fe7827dd81c574
Herman <hermantrym@gmail.com>
2022-12-30 11:22:02 +0700

    fixed

--- ChatAI2/ContentView.swift
+++ ChatAI2/ContentView.swift
@@ -2,20 +2,74 @@
 //  ContentView.swift
 //  ChatAI2
 //
-//  Created by admin on 29/12/22.
+//  Created by hermantrym on 29/12/22.
 //
 
+import OpenAISwift
 import SwiftUI
 
+final class ViewModel: ObservableObject {
+    init() {}
+    
+    private var client: OpenAISwift?
+    
+    func setup() {
+        client = OpenAISwift(authToken: "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")
+    }
+    
+    func send(text: String,
+              completion: @escaping (String) -> Void) {
+        client?.sendCompletion(with: text, maxTokens: 500, completionHandler: { result in
+            switch result {
+            case .success(let model):
+                let output = model.choices.first?.text ?? ""
+                completion(output)
+            case .failure:
+                break
+            }
+        })
+    }
+}
+
 struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+    @State var text = ""
+    @State var models = [String]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            ForEach(models, id: \.self) { string in
+                Text(string)
+            }
+            
+            Spacer()
+            
+            HStack {
+                TextField("Type here...", text: $text)
+                Button("Send") {
+                    send()
+                }
+            }
         }
+        .onAppear {
+            viewModel.setup()
+        }
+        
         .padding()
+    }
+    
+    func send() {
+        guard !text.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+        models.append("Me: \(text)")
+        viewModel.send(text: text) { response in
+            DispatchQueue.main.async {
+                self.models.append("ChatGPT: " + response)
+                self.text = ""
+            }
+        }
     }
 }
 
